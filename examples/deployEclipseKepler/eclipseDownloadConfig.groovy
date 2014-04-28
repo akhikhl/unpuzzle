ea3db847d4dd82903a8779b85d4fce44a358d7bb
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-04-28 17:36:04 +0200

    bumped version to 0.0.5, fixed examples

--- examples/deployEclipseKepler/eclipseDownloadConfig.groovy
+++ examples/deployEclipseKepler/eclipseDownloadConfig.groovy
@@ -1,27 +1,23 @@
 group = 'eclipse-kepler'
 
 def eclipseArchives = [
-  'linux_x86_32' : 'eclipse-jee-kepler-SR1-linux-gtk.tar.gz',
-  'linux_x86_64' : 'eclipse-jee-kepler-SR1-linux-gtk-x86_64.tar.gz',
-  'windows_x86_32' : 'eclipse-jee-kepler-SR1-win32.zip',
-  'windows_x86_64' : 'eclipse-jee-kepler-SR1-win32-x86_64.zip'
+  'linux_x86_32' : 'eclipse-jee-kepler-SR2-linux-gtk.tar.gz',
+  'linux_x86_64' : 'eclipse-jee-kepler-SR2-linux-gtk-x86_64.tar.gz',
+  'windows_x86_32' : 'eclipse-jee-kepler-SR2-win32.zip',
+  'windows_x86_64' : 'eclipse-jee-kepler-SR2-win32-x86_64.zip'
 ]
 
 def eclipseSdkArchives = [
-  'linux_x86_32' : 'eclipse-SDK-4.3.1-linux-gtk.tar.gz',
-  'linux_x86_64' : 'eclipse-SDK-4.3.1-linux-gtk-x86_64.tar.gz',
-  'windows_x86_32' : 'eclipse-SDK-4.3.1-win32.zip',
-  'windows_x86_64' : 'eclipse-SDK-4.3.1-win32-x86_64.zip'
+  'linux_x86_32' : 'eclipse-SDK-4.3.2-linux-gtk.tar.gz',
+  'linux_x86_64' : 'eclipse-SDK-4.3.2-linux-gtk-x86_64.tar.gz',
+  'windows_x86_32' : 'eclipse-SDK-4.3.2-win32.zip',
+  'windows_x86_64' : 'eclipse-SDK-4.3.2-win32-x86_64.zip'
 ]
-
-def eclipseMirror = 'http://mirror.netcologne.de'
 
 def eclipseArchive = eclipseArchives[current_os + '_' + current_arch]
 def eclipseSdkArchive = eclipseSdkArchives[current_os + '_' + current_arch]
 
-source "$eclipseMirror/eclipse//technology/epp/downloads/release/kepler/SR1/$eclipseArchive"
-source "$eclipseMirror/eclipse//eclipse/downloads/drops4/R-4.3.1-201309111000/$eclipseSdkArchive", sourcesOnly: true
-source "$eclipseMirror/eclipse//eclipse/downloads/drops4/R-4.3.1-201309111000/eclipse-4.3.1-delta-pack.zip"
+source "$eclipseMirror/eclipse//technology/epp/downloads/release/kepler/SR2/$eclipseArchive"
+source "$eclipseMirror/eclipse//eclipse/downloads/drops4/R-4.3.2-201402211700/$eclipseSdkArchive", sourcesOnly: true
+source "$eclipseMirror/eclipse//eclipse/downloads/drops4/R-4.3.2-201402211700/eclipse-4.3.2-delta-pack.zip"
 source "$eclipseMirror/eclipse//technology/babel/babel_language_packs/R0.11.1/kepler/BabelLanguagePack-eclipse-de_4.3.0.v20131123020001.zip", languagePacksOnly: true
-
-
