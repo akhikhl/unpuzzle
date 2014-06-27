9cd64ccff7e6a483b8880beafb9fa6290011140b
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-06-27 19:33:40 +0200

    added eclipse 4.4 configuration

--- libs/unpuzzle-plugin/src/main/resources/org/akhikhl/unpuzzle/defaultConfig.groovy
+++ libs/unpuzzle-plugin/src/main/resources/org/akhikhl/unpuzzle/defaultConfig.groovy
@@ -121,4 +121,24 @@ unpuzzle {
       languagePackTemplate '${eclipseMirror}/eclipse//technology/babel/babel_language_packs/R0.11.1/kepler/BabelLanguagePack-eclipse-${language}_4.3.0.v20131123020001.zip'
     }
   }
+
+  eclipseVersion('4.4') {
+
+    extendsFrom '4.3.2'
+
+    eclipseMavenGroup = 'eclipse-luna'
+
+    eclipseMirror = 'http://mirror.netcologne.de'
+
+    eclipseArchiveMirror = 'http://archive.eclipse.org'
+
+    sources {
+
+      source "$eclipseMirror/eclipse//technology/epp/downloads/release/luna/R/eclipse-jee-luna-R-${suffix_os[current_os]}${suffix_arch[current_arch]}.${fileExt_os[current_os]}"
+      source "$eclipseMirror/eclipse//eclipse/downloads/drops4/R-4.4-201406061215/eclipse-SDK-4.4-${suffix_os[current_os]}${suffix_arch[current_arch]}.${fileExt_os[current_os]}"
+      source "$eclipseMirror/eclipse//eclipse/downloads/drops4/R-4.4-201406061215/eclipse-4.4-delta-pack.zip"
+
+      languagePackTemplate '${eclipseMirror}/eclipse//technology/babel/babel_language_packs/R0.12.0/luna/BabelLanguagePack-eclipse-${language}_4.4.0.v20140623020002.zip'
+    }
+  }  
 }
